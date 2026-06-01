842210257e92cf17f12ce644c7a90ae78c762d44
Agent1c <simon.ndlovu1sa@gmail.com>
2026-06-01 23:18:25 +0200

    init: asserts files and setup

---
+++ asserts/js/contactForm.js
@@ -0,0 +1,5 @@
+const nameInput = document.querySelector('#nameInput')
+const phone = document.querySelector('#phone')
+const option = document.querySelector('option')
+const display = document.querySelector('.display')
+const button = document.querySelector('button');
